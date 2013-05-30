ad8e2f152fbbefe10b1d75334c731b2779f41caf
Taiki Kawakami <moznion@gmail.com>
2013-05-30 09:11:23 +0000

    Import of MOZNION/Test-LocalFunctions-0.10 from CPAN.
    gitpan-cpan-distribution: Test-LocalFunctions
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         MOZNION/Test-LocalFunctions-0.10.tar.gz
    gitpan-cpan-author:       MOZNION
    gitpan-cpan-maturity:     released

--- lib/Test/LocalFunctions/Util.pm
+++ lib/Test/LocalFunctions/Util.pm
@@ -4,42 +4,7 @@ use strict;
 use warnings;
 use ExtUtils::Manifest qw/maniread/;
 use Sub::Identify qw/stash_name/;
-
-sub all_local_functions_ok {
-    my ( $caller, %args ) = @_;
-
-    my $builder = $caller->builder;
-    my @libs = _list_modules_in_manifest($builder);
-
-    $builder->plan( tests => scalar @libs );
-
-    my $fail = 0;
-    for my $lib (@libs) {
-        test_local_functions( $caller, $builder, $lib, \%args ) or $fail++;
-    }
-
-    return $fail == 0;
-}
-
-sub test_local_functions {
-    my ( $caller, $builder, $file, $args ) = @_;
-
-    local $Test::Builder::Level = $Test::Builder::Level + 1;
-
-    my $pid = fork();
-    if ( defined $pid ) {
-        if ( $pid != 0 ) {
-            wait;
-            return $builder->ok( $? == 0, $file );
-        }
-        else {
-            exit $caller->is_in_use( $builder, $file, $args );
-        }
-    }
-    else {
-        die "failed forking: $!";
-    }
-}
+use Module::Load;
 
 sub list_local_functions {
     my $module = shift;
@@ -47,10 +12,11 @@ sub list_local_functions {
     my @local_functions;
 
     no strict 'refs';
+    load $module;
     while ( my ( $key, $value ) = each %{"${module}::"} ) {
         next unless $key =~ /^_/;
         next unless *{"${module}::${key}"}{CODE};
-        next if $module ne stash_name( $module->can($key) );
+        next unless $module eq stash_name( $module->can($key) );
         push @local_functions, $key;
     }
     use strict 'refs';
@@ -72,17 +38,5 @@ sub extract_module_name {
     }
 
     return $file;
-}
-
-sub _list_modules_in_manifest {
-    my $builder = shift;
-
-    if ( not -f $ExtUtils::Manifest::MANIFEST ) {
-        $builder->plan(
-            skip_all => "$ExtUtils::Manifest::MANIFEST doesn't exist" );
-    }
-    my $manifest = maniread();
-    my @libs = grep { m!\Alib/.*\.pm\Z! } keys %{$manifest};
-    return @libs;
 }
 1;
